396fa8f7bf71d44bba6338c23ba07355dcca788f
anurag shrinagar <anurag.shrinagar@oracle.com>
2019-01-05 16:41:45 +0530

    Heap sort implementation

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,5 @@
-module pkg1{
-    exports modulepkg1;
-    requires java.rmi; // It fails without mentioning this required module.ß
-}
+//module pkg1{
+//    exports modulepkg1;
+//    requires java.rmi; // It fails without mentioning this required module.ß
+//    requires kotlin.stdlib;
+//}
