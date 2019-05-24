854e17204b9f0a034ab383c097a8e5eb8d11e22c
anurag shrinagar <anurag.shrinagar@oracle.com>
2019-05-24 21:01:43 +0530

    simpler version of permutations

--- src/java/algos/Permutations2.java
+++ src/java/algos/Permutations2.java
@@ -5,28 +5,26 @@ public class Permutations2 {
     //Permutations using back-tracking.
 
     public static void main(String[] args) {
-        permute("abcd".toCharArray() ,4);
+        permute("abcd".toCharArray(), 0); // 0
     }
 
-    public static void permute(char[] cArr, int len){
-        if(len == 2){
+    public static void permute(char[] cArr, int len) {
+        if (len == 4) {
             System.out.println(String.valueOf(cArr));
-            swap(cArr, cArr.length-2, cArr.length-1);
-            System.out.println(String.valueOf(cArr));
-            swap(cArr, cArr.length-1, cArr.length-2);
+            return;
         }
-        else {
-            int i = cArr.length - len;
-            permute(cArr, len-1);
-            for(int j=i+1; j <cArr.length; j++){
-                swap(cArr,i,j);
-                permute(cArr, len-1);
-                swap(cArr,j,i);
-            }
+
+        int i = len; // 0
+        permute(cArr, len + 1);
+        for (int j = i + 1; j < cArr.length; j++) {
+            swap(cArr, i, j);
+            permute(cArr, len + 1);
+            swap(cArr, j, i);
         }
+
     }
 
-    private static void swap(char[] cArr, int i,int j){
+    private static void swap(char[] cArr, int i, int j) {
         char tmp = cArr[i];
         cArr[i] = cArr[j];
         cArr[j] = tmp;
