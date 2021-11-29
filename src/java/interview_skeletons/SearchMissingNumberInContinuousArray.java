03d845b20968965f1ae04a9982bb9f5b1f63ccd5
Anurag Shrinagar <ashrinagar@vmware.com>
2021-11-29 11:53:56 +0530

    Search missing number in continuous array

---
+++ src/java/interview_skeletons/SearchMissingNumberInContinuousArray.java
@@ -0,0 +1,14 @@
+package interview_skeletons;
+
+import java.io.IOException;
+
+/**
+ * Find a missing number from an array of contiguous numbers
+ * 1. Unsorted array
+ * 2. Sorted array
+ *
+ * 1 2 3 4 5 6 7 9
+ */
+
+public class SearchMissingNumberInContinuousArray {
+}
--- src/java/interview_skeletons/TwoSum.java
+++ src/java/interview_skeletons/TwoSum.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+ *               13 -> 14 ->
+ */
+
+/**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
