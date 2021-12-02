42bf0072f91f45d43f1ecf77c8d5711f0829bea0
Anurag Shrinagar <ashrinagar@vmware.com>
2021-12-02 09:02:46 +0530

    Search missing number in continuous array algorithm

--- src/java/interview_skeletons/SearchMissingNumberInContinuousArray.java
+++ src/java/interview_skeletons/SearchMissingNumberInContinuousArray.java
@@ -11,4 +11,34 @@ import java.io.IOException;
  */
 
 public class SearchMissingNumberInContinuousArray {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(findMissingNumber(new int[]{1,3}));
+    }
+
+    public static int findMissingNumber(int [] a){
+
+        if (a[0] != 1){
+            return 1;
+        }
+        if(a.length == 1 && a[0] ==1){
+            return 2;
+        }
+        int i = a.length /2; //4
+        int f =0;
+        int slice = a.length/2; //4
+        while( slice != 0){
+            slice /=2; // slice = 0
+            if(a[i] -1 == i){ //true
+                f = a[i]+1; // f = 8
+                i += slice;  // i = 7
+
+            } else{
+                f = a[i]-1;
+                i -= slice;
+            }
+        }
+
+        return f;
+    }
 }
